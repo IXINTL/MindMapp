861f9626db0b9a39286f730cd314c9cbc84b871a
Abhiraj Roy (iconized) <157954129+iconized@users.noreply.github.com>
2024-04-22 15:32:20 +0530

    create iconized push script!

---
+++ iconized.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Author            : Eshan Roy
+# Author II         : Abhiraj Roy
+# Author URI        : https://eshanized.github.io
+# Author URI II     : https://iconized.github.io
+
+# NOTE: Run this at your own risk! 
+
+# GitHub repository URL
+REPO_URL="https://github.com/IXINTL/MindMapp.git"
+
+# Check if there are any changes to commit
+if [[ -n $(git status -s) ]]; then
+    # Add all changes to the staging area
+    git add .
+
+    # Prompt user for commit message
+    read -p "Enter commit message: " COMMIT_MESSAGE
+
+    # Commit changes with a default message
+    git commit -m "$COMMIT_MESSAGE"
+
+    # Push changes to the remote repository
+    git push $REPO_URL
+
+    # Check if the push was successful
+    if [ $? -eq 0 ]; then
+        echo "Push successful!"
+    else
+        echo "Error: Push failed!"
+    fi
+else
+    echo "No changes to commit."
+fi
